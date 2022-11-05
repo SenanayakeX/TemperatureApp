28953d54dc41637fa754e6805aed34e5e307d9f1
Pasindu Senanayake <pasindusenanayake@iclod.com>
2022-11-06 04:47:56 +0530

    Codes files added!

---
+++ app/src/main/java/com/example/tute_2/Calculations.java
@@ -0,0 +1,16 @@
+package com.example.tute_2;
+
+public class Calculations {
+
+    protected float convertCelciusToFahrenheit(Float value) {
+        Float ans = (value * 9/5) + 32;
+        return ans;
+    }
+
+    protected float convertFahrenheitToCelcius(Float value) {
+        Float ans = (value - 32) * 5/9;
+        return ans;
+    }
+
+
+}
